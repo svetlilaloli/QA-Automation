7f62ce2cb186dba5f63492acf7b3654cd4e423e6
svetlilaloli <svetlilaloli@gmail.com>
2022-05-26 05:21:21 +0100

    Initial commit

---
+++ SummatorWithPOM/SummatorWithPOM/PageObjects/SumTwoNumbersPage.cs
@@ -0,0 +1,65 @@
+﻿using OpenQA.Selenium;
+using System;
+
+namespace SummatorWithPOM.PageObjects
+{
+    public class SumTwoNumbersPage
+    {
+        private readonly IWebDriver driver;
+        public SumTwoNumbersPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            //driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
+        }
+        public string PageUrl => "https://sum-numbers.nakov.repl.co/";
+        public IWebElement Heading => driver.FindElement(By.CssSelector("body > form > h1"));
+        public IWebElement FirstNumberLabelElement => driver.FindElement(By.CssSelector("body > form > div:nth-child(2) > label"));
+        public IWebElement SecondNumberLabelElement => driver.FindElement(By.CssSelector("body > form > div:nth-child(3) > label"));
+        public IWebElement FirstNumberInputElement => driver.FindElement(By.Id("number1"));
+        public IWebElement SecondNumberInputElement => driver.FindElement(By.Id("number2"));
+        public IWebElement CalculateButton => driver.FindElement(By.Id("calcButton"));
+        public IWebElement ResetButton => driver.FindElement(By.Id("resetButton"));
+        public IWebElement ResultDivElement => driver.FindElement(By.Id("result"));
+        public IWebElement ValidResultElement => driver.FindElement(By.CssSelector("#result > pre"));
+        public IWebElement InvalidResultElement => driver.FindElement(By.CssSelector("#result > i"));
+        public void Open()
+        {
+            driver.Url = PageUrl;
+        }
+        public void ResetForm()
+        {
+            ResetButton.Click();
+        }
+        public bool IsFormEmpty()
+        {
+            return FirstNumberInputElement.Text + SecondNumberInputElement.Text == "";
+        }
+        public void AddNumbers(string num1, string num2)
+        {
+            FirstNumberInputElement.SendKeys(num1);
+            SecondNumberInputElement.SendKeys(num2);
+            CalculateButton.Click();
+        }
+        public string GetResult()
+        {
+            string result;
+            try 
+            {
+                result = ValidResultElement.Text;
+            }
+            catch (Exception)
+            {
+                result =  InvalidResultElement.Text;
+            }
+            return result;
+        }
+        public string GetPageTitle()
+        {
+            return driver.Title;
+        }
+        public string GetPageHeading()
+        {
+            return Heading.Text;
+        }
+    }
+}
---
+++ SummatorWithPOM/SummatorWithPOM/Tests/SumTwoNumbersPageTests.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using SummatorWithPOM.PageObjects;
+
+namespace SummatorWithPOM.Tests
+{
+    public class SumTwoNumbersPageTests
+    {
+        private SumTwoNumbersPage page;
+        private IWebDriver driver;
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            driver = new ChromeDriver();
+            page = new SumTwoNumbersPage(driver);
+            page.Open();
+        }
+        [Test]
+        public void Test_Content()
+        {
+            Assert.AreEqual("Sum Two Numbers", page.GetPageTitle());
+            Assert.AreEqual("Sum Two Numbers", page.GetPageHeading());
+            Assert.AreEqual("", page.FirstNumberInputElement.Text);
+            Assert.AreEqual("", page.SecondNumberInputElement.Text);
+            Assert.AreEqual("Calculate", page.CalculateButton.GetAttribute("value"));
+            Assert.AreEqual("Reset", page.ResetButton.GetAttribute("value"));
+            Assert.AreEqual("", page.ResultDivElement.Text);
+        }
+        // valid input
+        [TestCase("12345678901234563456788888", "1234567890123456789", "1.2345680135802454e+25")]
+        [TestCase("0", "0", "0")]
+        // invalid input
+        [TestCase("", "78", "invalid input")]
+        [TestCase("0.123", "", "invalid input")]
+        [TestCase("--3.45", "89", "invalid input")]
+        [TestCase("-6.35", "++5678", "invalid input")]
+        [TestCase("asd", "7856", "invalid input")]
+        [TestCase("677", "dsa", "invalid input")]
+        public void Test_SumInvalidInput(string num1, string num2, string expectedResult)
+        {
+            page.ResetForm();
+            page.AddNumbers(num1, num2);
+            Assert.AreEqual($"{expectedResult}", page.GetResult());
+        }
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            driver.Quit();
+        }
+    }
+}
